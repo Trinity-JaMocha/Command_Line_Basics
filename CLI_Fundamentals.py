e0701f0373592095ddc4967c24833d7174dd2a22
Trinity-JaMocha <trinityblush@gmail.com>
2026-03-03 19:22:31 +0000

    Intro_to_CLI

---
+++ CLI_Fundamentals.py
@@ -0,0 +1,9 @@
+ # Command Line = a text-based user interface used to interact with a computer's operating system by typing commands at a prompt. 
+  # Unlike a graphical user interface (GUI), which uses a mouse to click on icons and menus, the command line relies solely on the keyboard 
+  # for input and displays output as text.
+
+# Why type?
+ # File Management
+ # Control Development Environment
+ # Automate Repetitive Tasks
+
