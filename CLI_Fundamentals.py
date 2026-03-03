a9ebe6e257acdb04059dcfe82957b9ca942dd958
Trinity-JaMocha <trinityblush@gmail.com>
2026-03-03 19:50:33 +0000

    File Tree Commands

--- CLI_Fundamentals.py
+++ CLI_Fundamentals.py
@@ -1,3 +1,5 @@
+ # 1.6hrs on SCRIMBA
+ 
  # Command Line = a text-based user interface used to interact with a computer's operating system by typing commands at a prompt. 
   # Unlike a graphical user interface (GUI), which uses a mouse to click on icons and menus, the command line relies solely on the keyboard 
   # for input and displays output as text.
@@ -7,3 +9,28 @@
  # Control Development Environment
  # Automate Repetitive Tasks
 
+-----------------------------------------------------
+
+# INSPECT THE FILE TREE
+
+pwd = # pwd is solely for displaying your current location; it cannot be used to change directories. The cd command is used for that purpose.
+
+ls = # a fundamental utility in Unix-like operating systems used to list the contents of a directory.
+
+cd = # short for change directory, is a fundamental shell command used to navigate and change the current working directory in command-line 
+ # interface
+
+clear = # used to clear the terminal screen, improving readability by removing previous output. No actions are stopped, just visual clutter.
+
+///////
+
+Challenge: Inspect the file tree
+1. Print the current working directory.
+2. Navigate into the directory "geography_game".
+3. List the contents of "geography_game".
+4. Clear the terminal.
+
+# 1 --- pwd
+# 2 --- ls
+# 3 --- cd geography_game --- (afterwards) ls (names contents in geography_game)
+# 4 --- clear
