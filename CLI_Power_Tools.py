3d104bfaae4826371c7cf083622353135a682b4e
Trinity-JaMocha <trinityblush@gmail.com>
2026-03-04 22:51:48 +0000

    Linux and Shell

--- CLI_Power_Tools.py
+++ CLI_Power_Tools.py
@@ -1,1 +1,18 @@
 # TERMINOLOGY CRASH COURSE
+
+Linux
+ # Open-Source & Free: The source code is free to study, modify, and redistribute, fostering a massive collaborative community.
+ # The Kernel: At its core, the Linux kernel manages system resources, hardware, and memory.
+ # Distributions (Distros): Instead of one single OS, users choose "flavors" (distros) that bundle the kernel with software, 
+  # desktop environments, and tools tailored for specific needs (e.g., gaming, server management, or desktop use).
+
+Shell
+ # Command-Line Interpreter: The shell reads user input, parses it, and executes the requested programs.
+ # Interaction Type: While often a Command-Line Interface (CLI), shells can also refer to Graphical User Interfaces (GUI).
+ # Functionality: Beyond executing commands, shells can be used for file manipulation, environment variable configuration, 
+  # and automation via scripting.
+ # Automation: Users can store sequences of commands in a file (a shell script) to automate repetitive tasks. 
+
+ -----------------------------------------------------
+
+ 
